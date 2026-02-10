58eb84fc420b97094a3217b17feac93e1cc9d31c
ariannysOronoz <ariom0910@gmail.com>
2026-02-10 16:04:14 -0300

    style: update VerticalText component to use MUI Typography

--- src/components/aboutMe/verticalText/VerticalText.js
+++ src/components/aboutMe/verticalText/VerticalText.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './VerticalText.css';
+import { Box, Typography } from '@mui/material';
 
 export default function VerticalText({ description }){
     return(
-        <div className="VerticalText-folder-container">
-            <div className="VerticalText-folder-item folder-description">
+        <Box sx={{ p: { xs: 2, md: 4 } }}>
+            <Typography variant="h4" sx={{ fontWeight: 800, mb: 3, color: '#1e293b' }}>
+                Sobre mí
+            </Typography>
+            <Typography variant="body1" sx={{ color: '#475569', lineHeight: 1.8, fontSize: '1.1rem' }}>
                 {description}
-            </div>
-        </div>
-    )
+            </Typography>
+        </Box>
+    );
 }
